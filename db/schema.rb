323f0c3c419b4919d48737da147690e5293189cb
Eric Chiu <c3366301@email.com>
2019-10-31 22:53:22 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_113349) do
+ActiveRecord::Schema.define(version: 2019_10_31_145236) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
